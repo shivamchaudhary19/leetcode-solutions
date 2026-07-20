62bc1a973888fe17c73a1c4dc8b125d5b820f6d9
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-07-20 21:50:40 +0530

    feat: solve LeetCode #11 using two pointers

---
+++ arrays/11-container-with-most-water.js
@@ -0,0 +1,22 @@
+var maxArea = function (height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+
+    while (left < right) {
+
+        let width = right - left;
+
+        let area = width * Math.min(height[left], height[right]);
+
+        maxArea = Math.max(maxArea, area);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxArea;
+};
