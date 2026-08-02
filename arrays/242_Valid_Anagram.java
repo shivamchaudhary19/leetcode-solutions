6ffdd129ccab0d2ec0de631be58aa26844008742
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-08-02 23:21:34 +0530

    Add Java solution for LeetCode 242: Valid Anagram

---
+++ arrays/242_Valid_Anagram.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] freq = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        for (char ch : t.toCharArray()) {
+            if (freq[ch - 'a'] > 0) {
+                freq[ch - 'a']--;
+            } else {
+                return false;
+            }
+        }
+
+        for (int count : freq) {
+
+            if (count != 0) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+}
