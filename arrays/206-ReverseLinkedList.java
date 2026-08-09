021b1a8fd83d1dd852e67620aa15f3fa4ee54861
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-08-09 23:40:07 +0530

    Added ListNode class to leetcode #206 to make it correct

--- arrays/206-ReverseLinkedList.java
+++ arrays/206-ReverseLinkedList.java
@@ -1,3 +1,20 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 class Solution {
     public ListNode reverseList(ListNode head) {
 
