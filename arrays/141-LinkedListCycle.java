b1198732a165ce851b7143ac4f90bbf53ba69626
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-08-10 22:13:38 +0530

    Solve LeetCode 141 using fast and slow pointers

---
+++ arrays/141-LinkedListCycle.java
@@ -0,0 +1,35 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+// Here we defined the linked list , only needed in code editors , no need of it in leetcode
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
