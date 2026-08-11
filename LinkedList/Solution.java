46cbd3593ea4aa7d83ad61a9f6dd03ff8e8a3e58
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-08-11 23:35:06 +0530

    Solve LeetCode 142 using Floyd's cycle detection

---
+++ LinkedList/Solution.java
@@ -0,0 +1,46 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+// Here we defined the linked list , only needed in code editors , no need of it in leetcode
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        ListNode start = head;
+
+        while (start != slow) {
+            start = start.next;
+            slow = slow.next;
+        }
+
+        return start;
+    }
+}
