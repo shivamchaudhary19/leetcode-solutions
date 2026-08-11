430b330d4eee657734806c92ee62493f799c89e3
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-08-11 12:10:05 +0530

    Solve LeetCode 21 using two pointers

---
+++ LinkedList/21-MergeTwoSortedLists.java
@@ -0,0 +1,44 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+// Here we defined the linked list , only needed in code editors , no need of it in leetcode
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode temp = new ListNode(-1);
+        ListNode current = temp;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return temp.next;
+    }
+}
