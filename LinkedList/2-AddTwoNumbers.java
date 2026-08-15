4a09c65949f4d3c48a54f16a48d8703d47d108af
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-08-15 22:10:09 +0530

    Solve LeetCode 2 using linked list addition

---
+++ LinkedList/2-AddTwoNumbers.java
@@ -0,0 +1,49 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+// Here we defined the linked list , only needed in code editors , no need of it in leetcode
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int sum = val1 + val2 + carry;
+
+            current.next = new ListNode(sum % 10);
+
+            carry = sum / 10;
+
+            current = current.next;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
