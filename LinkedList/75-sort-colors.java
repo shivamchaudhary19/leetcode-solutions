a7c1971ef4175b920a49918bc2553b3cbf8abea5
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-08-18 23:54:25 +0530

    Solve LeetCode 75 using three pointers

---
+++ LinkedList/75-sort-colors.java
@@ -0,0 +1,46 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+// Here we defined the linked list , only needed in code editors , no need of it in leetcode
+
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+
+                low++;
+                mid++;
+
+            } else if (nums[mid] == 1) {
+                mid++;
+
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+
+                high--;
+            }
+        }
+    }
+}
