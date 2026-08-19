7ab04adc8c6e734486b57f262e08819bd3973b22
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-08-19 22:21:34 +0530

    Solve LeetCode 88 using two pointers

--- LinkedList/75-sort-colors.java
+++ LinkedList/75-sort-colors.java
@@ -1,18 +1,4 @@
-class ListNode {
-    int val;
-    ListNode next;
 
-    ListNode(int val) {
-        this.val = val;
-        this.next = null;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-// Here we defined the linked list , only needed in code editors , no need of it in leetcode
 
 class Solution {
     public void sortColors(int[] nums) {
