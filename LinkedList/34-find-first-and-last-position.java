6297b96edb71b0ba831cbd1c7d5ed35c7240bf59
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-08-27 23:59:06 +0530

    Solve LeetCode 34 using binary search

---
+++ LinkedList/34-find-first-and-last-position.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+
+        return new int[]{first, last};
+    }
+
+    private int findFirst(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                result = mid;
+                right = mid - 1;
+            } 
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    private int findLast(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                result = mid;
+                left = mid + 1;
+            } 
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
