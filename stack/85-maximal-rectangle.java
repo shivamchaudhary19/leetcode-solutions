51f73bd48ba8df03d692b3a9fe8cf51eaaaf2e08
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-09-24 23:39:41 +0530

    Solve LeetCode 85 using histogram and monotonic stack

---
+++ stack/85-maximal-rectangle.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[] heights = new int[cols];
+        int maxArea = 0;
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+                    heights[j]++;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
+        }
+
+        return maxArea;
+    }
+
+    private int largestRectangleArea(int[] heights) {
+
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+
+        for (int i = 0; i <= n; i++) {
+
+            int currentHeight = (i == n) ? 0 : heights[i];
+
+            while (!stack.isEmpty() &&
+                   heights[stack.peek()] > currentHeight) {
+
+                int height = heights[stack.pop()];
+                int width;
+
+                if (stack.isEmpty()) {
+                    width = i;
+                } else {
+                    width = i - stack.peek() - 1;
+                }
+
+                int area = height * width;
+                maxArea = Math.max(maxArea, area);
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
